7ff1a2459d2c2f7f5abfcaec133cdbdb3e481247
Walker Flieller <wflieller@gmail.com>
2016-01-18 12:42:16 -0600

    Added profile, settings,trending, update profile, watchlist pages, added routes, styled pages, updated styles, added stock exchanges collections

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,22 +1,57 @@
 angular
-  .module('stockAiApp')
-  .config(config);
- 
+    .module('stockAiApp')
+    .config(config);
+
 function config($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'client/templates/tabs.html'
-    })
-    .state('tab.stocks', {
-      url: '/stocks',
-      views: {
-        'tab-stocks': {
-          templateUrl: 'client/templates/stocks.html'
-        }
-      }
-    });
- 
-  $urlRouterProvider.otherwise('tab/stocks');
-}
+    $stateProvider
+        .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'client/templates/tabs.html'
+        })
+        .state('tab.stocks', {
+            url: '/stocks',
+            views: {
+                'tab-stocks': {
+                    templateUrl: 'client/templates/stocks.html'
+                }
+            }
+        })
+        .state('tab.settings', {
+            url: '/settings',
+            views: {
+                'tab-settings': {
+                    templateUrl: 'client/templates/settings.html'
+                }
+            }
+        })
+        .state('tab.watchlist', {
+            url: '/watchlist',
+            views: {
+                'tab-watchlist': {
+                    templateUrl: 'client/templates/watchlist.html'
+                }
+            }
+        })
+        .state('tab.profile', {
+            url: '/profile',
+            views: {
+                'tab-profile': {
+                    templateUrl: 'client/templates/profile.html'
+                }
+            }
+        })
+        .state('tab.trending', {
+            url: '/trending',
+            views: {
+                'tab-trending': {
+                    templateUrl: 'client/templates/trending.html'
+                }
+            }
+        })
+
+
+};
+
+$urlRouterProvider.otherwise('tab/stocks');
+
